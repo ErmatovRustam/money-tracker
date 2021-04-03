02cad86895ccdee05e651d0e9441d0f77cc7c0cc
Rustam Ermatov <ermatov_r@auca.kg>
2021-04-03 21:01:09 +0600

    add backColor for balance section

--- src/App.js
+++ src/App.js
@@ -6,26 +6,41 @@ function App() {
   const [balance, setBalance] = useState(0);
   const [popUp, setPopUp] = useState(false);
   const [isbackColorGr, setBackColorGr] = useState(true);
-
-  const decBtnHandler = (amount) => {
-    const finalAmount = balance - amount;
-    setBalance(finalAmount)
-    setBackColorGr(false)
-    setPopUp(true)
-  }
-
+  const [balanceColor, setBalanceColor] = useState('yelow')
+  
   const incBtnHandler = (amount) => {
-    const finalAmount = balance + amount;
-    setBalance(finalAmount)
-    setBackColorGr(true)
     setPopUp(true)
+    setBackColorGr(true)
   }
+    const decBtnHandler = (amount) => {
+    setPopUp(true)
+    setBackColorGr(false)
+  }
+
+  const countHandler = (val, tag) => {
+    if (isbackColorGr) {
+      const finalAmount = Number(balance) + Number(val);
+      setBalance(finalAmount)
+      checkBalanceColor(finalAmount)
+    } else {
+      const finalAmount = Number(balance) - Number(val);
+      setBalance(finalAmount)
+      checkBalanceColor(finalAmount)
+    }
+    setPopUp(false);
+    
+  };
 
   const togglePop = () => {
    setPopUp(false);
   };
 
-
+  const checkBalanceColor = (bal) =>{
+    if (bal > 0) setBalanceColor('#c2ebd3')
+    if (bal < 0) setBalanceColor('#e9c3c3')
+    if (bal === 0) setBalanceColor('#f0f5b1')
+  }
+  
   return(
     <div className="App">
       <h1>My Cash App</h1>
@@ -34,10 +49,10 @@ function App() {
           <button onClick = { () => decBtnHandler(10)} className='btn' id='dec-btn'>-</button>
           <button onClick = {() => incBtnHandler(10)} className='btn' id='inc-btn'>+</button>    
         </div>
-        <h3 id='balance'>{balance}</h3>
+        <h3 id='balance' style={{ backgroundColor : balanceColor } } >$ {balance} </h3>
         {
           popUp ? <div className="pop-up" >
-            <PopUp click={ togglePop } backColor = {isbackColorGr} />
+            <PopUp click={togglePop} backColor={isbackColorGr} count={countHandler}/>
           </div> : null
         }
       </div>
--- src/PopUp.js
+++ src/PopUp.js
@@ -1,7 +1,9 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './PopUp.css'
 
 function PopUp(props) {
+  const [value, setValue] = useState(0);
+  const [tag, setTag] = useState('');
   const handleClick = () => {
     props.click()
   }
@@ -10,18 +12,25 @@ function PopUp(props) {
     backgroundColor: 'red'
   }
 
+  const tagChanger = (e) => {
+    setTag(e.target.value)
+  }
+
+  const valChanger = (e) => {
+    setValue(e.target.value)
+  }
+
   return (
     <div className="modal_content" style={{ backgroundColor: props.backColor ? '#a0e1bb' : '#f7adac' }}>
-      {console.log(props.backColor)}
           <span className="close" onClick={handleClick}>  &times;{' '}</span>
           <div className = 'form-content'>
             <form id = 'form'>
                     <label for = 'amount'> Amount </label>
-                    <input type="number" id="amount" />
+          <input type="number" id="amount" placeholder = {0} onChange = {valChanger} hint = 'please enter number' />
                     <label for = 'name'> Tag name </label>
-                    <input type="text" id="name" />
+          <input type="text" id="name" placeholder = 'Rent'  value={tag } onChange={tagChanger} hint = 'please enter name for the amount ' />
                     <br />
-                    <button>Submit</button>
+          <button onClick={() => props.count(value, tag)}>Submit</button>
                 </form>
           </div>
           
